c6b60b61de11613f2b2e647c3875e5679675c183
Flavio Muscente <flaviomuscente@libero.it>
2020-12-11 10:33:04 +0100

    layout complete

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//attivazione di vue tools web
+Vue.config.devtools = true;
+//struttura generale vue
+var app = new Vue({
+  el: '#app',
+  data: {
+    langSelect: "en"
+  },
+  computed:{
+
+    }
+});
