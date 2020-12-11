7dc12d8397596e9464a8f7a61f9b1ad6a491ab19
Flavio Muscente <flaviomuscente@libero.it>
2020-12-11 15:58:10 +0100

    project finish

--- js/main.js
+++ js/main.js
@@ -4,9 +4,30 @@ Vue.config.devtools = true;
 var app = new Vue({
   el: '#app',
   data: {
-    langSelect: "en"
+    navItems:["Home", "Pages", "Courses", "Features", "Blog", "Shop"],
+    langSelect: "en",
+    pageDown: false
   },
-  computed:{
-
+  methods: { 
+    scrollToTop() {
+         window.scrollTo(0,0);
+    },
+    scroll() {
+      window.onscroll = () => {
+        if(document.documentElement.scrollTop > window.innerHeight){
+          this.pageDown = true;
+        } else{
+          this.pageDown = false;
+        }
+        console.log("offsetHeight"+document.documentElement.offsetHeight);
+        console.log("scrolltop" +document.documentElement.scrollTop)
+        
+        
+      };
     }
+        
+ },
+ mounted(){
+   this.scroll()
+ }
 });
